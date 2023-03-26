ce9019a7bacc00b73c99fbceea1c87c53dcdf5b0
biso150 <biso15@hanmail.net>
2023-03-27 05:24:50 +0900

    commit

--- src/init.js
+++ src/init.js
@@ -8,7 +8,8 @@ import app from "./server"
 // listening 할 포트(, 콜백 함수 설정)
 //const PORT = 4000;
 
-const handleServer = () => console.log(`Server listening on port http://localhost:${PORT}`);
+//const handleServer = () => console.log(`Server listening on port http://localhost:${PORT}`);
+const handleServer = () => console.log(`Server listening on`);
 
 // app.listen(PORT, handleServer);
 app.listen(process.env.PORT || 4000, handleServer);
